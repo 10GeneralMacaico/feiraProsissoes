eb14a7c3be4ea352371b8225f18a7602fda6d437
gustavo max. <ra54667252855@acaonsfatima.org.br>
2025-09-19 19:08:30 -0300

    Update main.jsx

--- epsilon/frontend/src/main.jsx
+++ epsilon/frontend/src/main.jsx
@@ -2,11 +2,12 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import React from 'react';
 
-import App from './home/App.jsx'
-import For from './pages/formularios/for.jsx'
-import ForS from './pages/cadastro/forS.jsx'
-import Log from './pages/login/Login.jsx'
-import QrCode from './pages/qrcodePage/qrcodePG.jsx'
+import App from './home/App.jsx';
+import For from './pages/formularios/for.jsx';
+import Curs from './pages/cursos/cursos.jsx';
+import ForS from './pages/cadastro/forS.jsx';
+import Log from './pages/login/Login.jsx';
+import QrCode from './pages/qrcodePage/qrcodePG.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render (
   <React.StrictMode>
@@ -15,6 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render (
         <Route path="/" element={<App />} />
         <Route path="/for" element={<For />} />
         <Route path="/cadastro" element={<ForS />}/>
+        <Route path="/cursos" element={<Curs />}/>
         <Route path="/login" element={<Log />}/>
         <Route path="/QR" element={<QrCode />}/>
       </Routes>
